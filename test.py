fe43196a11d572dfd2be232f0bda3bb270ba2afb
Kimnabin <ducvucong01@gmail.com>
2024-06-23 13:55:41 +0900

    Day 10 - Valid_Parentheses

--- test.py
+++ test.py
@@ -1,30 +1,20 @@
-class Solution:
+class Solution: 
+    def isValid(self, s):
+        stack = []
+        for c in s:
+            if c in '([{':
+                stack.append(c)
+            else:
+                if not stack or \
+                    (c == ')' and stack[-1] != '(') or \
+                    (c == ']' and stack[-1] != '[') or \
+                    (c == '}' and stack[-1] != '{'):
+                    return False
+                stack.pop()
+        return not stack
     
-    def __init__(self, nums):
-        self.nums = nums
-        self.longest = 0
-    
-    
-    def longestCunsecutive(self, nums):
-        nums = set(nums)
-        longest = 0
-        
-        for num in nums:
-            if num - 1 not in nums:
-                current_num = num
-                current_length = 1
-                
-                while current_num + 1 in nums:
-                    current_num += 1
-                    current_length += 1
-                    
-                longest = max(longest, current_length)
-                
-        return longest
-    
-    
-nums = [100, 4, 200, 1, 3, 2]
+str = '[)]){}'
 
-s = Solution(nums).longestCunsecutive(nums)
-
-print(s)
+solution = Solution()
+slt1 = solution.isValid(str)
+print(slt1)
