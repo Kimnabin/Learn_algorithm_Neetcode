754588dd75609dbd58c164e1625ecf1661779807
Kimnabin <ducvucong01@gmail.com>
2024-06-26 16:09:55 +0900

    Day 13 - Generate Parentheses

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
-class Solution: 
-    def isValid(self, s):
-        stack = []
-        for c in s:
-            if c in '([{':
-                stack.append(c)
-            else:
-                if not stack or \
-                    (c == ')' and stack[-1] != '(') or \
-                    (c == ']' and stack[-1] != '[') or \
-                    (c == '}' and stack[-1] != '{'):
-                    return False
-                stack.pop()
-        return not stack
-    
-str = '[)]){}'
+class Solution:
+    def generateParentheses(self, n):
+        result = []
+        queue = [(0, 0, '')]  # Khởi tạo hàng đợi với số lượng dấu ngoặc trái, phải và chuỗi hiện tại
+        
+        while queue:
+            left, right, s = queue.pop()
+            
+            # Nếu chuỗi đạt độ dài 2*n, thêm vào kết quả
+            if len(s) == 2 * n:
+                result.append(s)
+                continue
+            
+            # Nếu số lượng dấu ngoặc trái hiện tại nhỏ hơn n, thêm dấu ngoặc trái mới
+            if left < n:
+                queue.append((left + 1, right, s + '('))
+                
+            # Nếu số lượng dấu ngoặc phải hiện tại nhỏ hơn số lượng dấu ngoặc trái, thêm dấu ngoặc phải mới
+            if right < left:
+                queue.append((left, right + 1, s + ')'))
+        
+        return result
 
-solution = Solution()
-slt1 = solution.isValid(str)
-print(slt1)
+# Test với n = 3
+n = 3
+print(Solution().generateParentheses(n))  
+
